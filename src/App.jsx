e3ec52807b0f63253ed9463d6492c8292ccb8639
Izien Iremiren <izieniremiren@gmail.com>
2023-10-07 14:10:41 -0400

    refactored design to use CSS grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import qrCode from "./assets/qr-code.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="card main">
-      <img className="card" src={qrCode} />
-      <p className="title">
-        Improve your front-end skills by building projects
-      </p>
-      <p className="content">
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
+    <div className="container">
+      <div className="item1">
+        <img src={qrCode} />
+      </div>
+      <div className="item2">
+        <p className="title">
+          Improve your front-end skills by building projects
+        </p>
+      </div>
+      <div className="item3">
+        <p className="content">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </div>
   );
 }
